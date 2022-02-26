fcec8ad665dac84c695fd8435f2d18d118443770
alhafidz23 <80781083+alhafidz23@users.noreply.github.com>
2022-02-26 13:50:20 +0700

    main.js --youtap assess

---
+++ main.js
@@ -0,0 +1,49 @@
+function Solution(str)
+{
+
+    let stack = [];
+ 
+    for(let i = 0; i < str.length; i++)
+    {
+        let x = str[i];
+ 
+        if (x == '(' || x == '[' || x == '{')
+        {
+            stack.push(x);
+            continue;
+        }
+ 
+        if (stack.length == 0)
+            return false;
+             
+        let check;
+        switch (x){
+        case ')':
+            check = stack.pop();
+            if (check == '{' || check == '[')
+                return false;
+            break;
+ 
+        case '}':
+            check = stack.pop();
+            if (check == '(' || check == '[')
+                return false;
+            break;
+ 
+        case ']':
+            check = stack.pop();
+            if (check == '(' || check == '{')
+                return false;
+            break;
+        }
+    }
+ 
+    return (stack.length == 0);
+}
+ 
+let str = "()";
+ 
+if (Solution(str))
+    console.log(true)
+else
+    console.log(false);
